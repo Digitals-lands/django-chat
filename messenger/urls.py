08bfece5fa19fdab31ae50bd410faac96f27fb4e
MD <mariesantanna114@gmail.com>
2024-03-18 13:55:59 +0100

    Inscription et mise en place du socket

--- messenger/urls.py
+++ messenger/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from messenger import urls
 from .views import *
 urlpatterns = [
-    
-    path('', register),
+    path('', register, name='register'),
+    path('chat', chat_message, name='chat'),
 ]
