60d6c6a0c9d1daf090d1bb01239947733018b99f
Kdigit <dalmeidakevin910@gmail.com>
2024-03-18 17:19:58 +0100

    user

--- messenger/urls.py
+++ messenger/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from messenger import urls
 from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('', register, name='register'),
     path('chat', chat_message, name='chat'),
     path('login', login_user, name='login'),
     path('deconnexion',deconnexion,name='deconnexion')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
